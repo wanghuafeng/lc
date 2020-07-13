5450034ef0cc77d93ccfd47716610e42c7d5c186
wanghuafeng <sivilwang@163.com>
2020-07-13 17:31:44 +0800

    88.合并两个有序数组

---
+++ 88.合并两个有序数组.py
@@ -0,0 +1,49 @@
+#!-*- coding:utf-8 -*-
+"""
+https://leetcode-cn.com/problems/merge-sorted-array/
+
+给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+ 
+
+说明:
+    初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。
+    你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+
+示例:
+    输入:
+        nums1 = [1,2,3,0,0,0], m = 3
+        nums2 = [2,5,6],       n = 3
+    输出: [1,2,2,3,5,6]
+"""
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        l = []
+        p1 = 0
+        p2 = 0
+        while p1 < m and p2 < n:
+            if nums1[p1] < nums2[p2]:
+                l.append(nums1[p1])
+                p1 += 1
+            else:
+                l.append(nums2[p2])
+                p2 += 1
+        l = l + nums1[p1:m] + nums2[p2:n]
+        for i in nums1: # 重新赋值
+            nums1[i] = l[i]
+
+
+
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+
+print Solution().merge(nums1, m, nums2, n)
