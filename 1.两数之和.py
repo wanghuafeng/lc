ea90d3dff600667b8e07914f42a6a9ca2b68c0d5
no one <wanghuafengc@163.com>
2020-07-29 22:14:55 +0800

    1.两数之和

---
+++ 1.两数之和.py
@@ -0,0 +1,33 @@
+#!-*- coding:utf-8 -*-
+"""
+https://leetcode-cn.com/problems/two-sum/
+
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+
+示例:
+    给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+"""
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hash_table = {}
+        for i in range(len(nums)):
+            num = nums[i]
+            if num in hash_table:
+                return [hash_table.get(num), i]
+            else:
+                hash_table[target-num] = i
+
+nums = [2, 7, 11, 15]
+target = 9
+print Solution().twoSum(nums, target)
