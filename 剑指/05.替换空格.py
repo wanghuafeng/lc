f4dda075939f8c22807fb6bccbe2683eaa3883e4
wanghuafeng <sivilwang@163.com>
2020-08-18 10:28:27 +0800

    05.替换空格

---
+++ 剑指/05.替换空格.py
@@ -0,0 +1,29 @@
+#!-*- coding:utf-8 -*-
+"""
+https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/
+请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+
+示例 1：
+    输入：s = "We are happy."
+    输出："We%20are%20happy."
+限制：
+    0 <= s 的长度 <= 10000
+"""
+
+class Solution(object):
+    def replaceSpace(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res = []
+        for char in s:
+            if char == ' ':
+               res.append('%20')
+            else:
+                res.append(char)
+        return ''.join(res)
+
+s = " 1  "
+print s.split()
+print Solution().replaceSpace(s)
