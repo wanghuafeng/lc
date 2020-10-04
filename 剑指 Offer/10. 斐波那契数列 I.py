08d2c0a2dd4abd01f8ee7b5ac26bde7cef43eff4
wanghuafeng <sivilwang@163.com>
2020-10-04 22:18:15 +0800

    斐波那契

--- 剑指 Offer/10. 斐波那契数列 I.py
+++ 剑指 Offer/10. 斐波那契数列 I.py
@@ -25,3 +25,28 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        if n == 0 or n == 1:
+            return n
+        a = 1
+        b = 2
+        for i in range(2, n):
+            a, b = b, a+b
+        return a % 1000000007
+
+
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0 or n == 1:
+            return n
+        dp = [0 for _ in range(n)]
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[-1] % 1000000007
+
+print Solution().fib(6)
