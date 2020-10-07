798209ebfcd095e427046899c692879fd575a4b5
wanghuafeng <sivilwang@163.com>
2020-10-07 16:52:22 +0800

    pow(x, n)

--- 50.Pow(x, n).py
+++ 50.Pow(x, n).py
@@ -21,6 +21,26 @@ https://leetcode-cn.com/problems/powx-n/
     1. n的正负问题
     2. n的奇偶问题
 """
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if x == 0:
+            return 0
+        if n < 0:
+            x = 1/x
+            n = -n
+        res = 1
+        while n:
+            if n % 2 == 1:  # 奇数
+                res *= x
+            x *= x
+            n /= 2
+        return res
+print Solution().myPow(2.0, -2)
 
 class Solution(object):
     def myPow(self, x, n):
